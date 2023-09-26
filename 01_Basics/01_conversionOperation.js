e6bf964724cc4bcd7a2bf0bfb73a57717cdc5bc0
Pavan Amrut Badgujar <pavanabad01@gmail.com>
2023-09-25 20:16:02 -0700

    add in datatype-summary types of memory

---
+++ 01_Basics/01_conversionOperation.js
@@ -0,0 +1,36 @@
+let someNumber=33
+let strigNumber=String(someNumber)
+// console.log(typeof strigNumber)// {33 print type of String}
+
+// ************************ Operations ***************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3)
+
+
+// console.log("1"+ 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2 )
+// console.log(1 + 2 + "2" )
+
+// console.log((3 + 4) * 5 % 3)
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 100
+gameCounter++
+++gameCounter
+console.log(gameCounter)
---
+++ 01_Basics/02_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+// console.log(null>0)
+// console.log(null == 0)
+// console.log(null >= 0 )
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+// ===
+console.log("10" == 10) // true here only checks value if value is equal then true
+console.log("10" === 10)  // false here value or data type also check
